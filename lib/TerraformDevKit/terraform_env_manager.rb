0d9e2c049b7322b818a8e710b9711290aafb1793
Victor Jimenez <vjimenez@vistaprint.com>
2017-06-23 10:10:02 +0200

    Filter terraform output from terraform environment manager.

--- lib/TerraformDevKit/terraform_env_manager.rb
+++ lib/TerraformDevKit/terraform_env_manager.rb
@@ -4,18 +4,20 @@ require 'TerraformDevKit/os'
 module TerraformDevKit
   class TerraformEnvManager
     def self.exist?(env)
-      output = Command.run('terraform env list')
+      output = Command.run('terraform env list', print_output: false)
       output.any? { |line| line.tr('* ', '') == env }
     end
 
     def self.create(env)
-      Command.run("terraform env new #{env}") unless exist?(env)
+      unless exist?(env)
+        Command.run("terraform env new #{env}", print_output: false)
+      end
     end
 
     def self.delete(env)
       if exist?(env)
         select('default')
-        Command.run("terraform env delete #{env}")
+        Command.run("terraform env delete #{env}", print_output: false)
       end
     rescue RuntimeError => error
       # TODO: Get rid of this hack once the following issue gets fixed:
@@ -26,11 +28,11 @@ module TerraformDevKit
 
     def self.select(env)
       create(env)
-      Command.run("terraform env select #{env}")
+      Command.run("terraform env select #{env}", print_output: false)
     end
 
     def self.active
-      output = Command.run('terraform env list')
+      output = Command.run('terraform env list', print_output: false)
       active = output.select { |line| line.include?('*') }
       raise 'Error parsing output from terraform' if active.length != 1
       match = /\s*\*\s*(\S+)/.match(active[0])
