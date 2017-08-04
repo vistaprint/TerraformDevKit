63d602aacb1ed098eb11cf0a411a691a4d4a4ebe
Victor Jimenez <vjimenez@vistaprint.com>
2017-08-04 12:01:48 +0200

    Add better support for retrying requests. Fixes #2

--- TerraformDevKit.gemspec
+++ TerraformDevKit.gemspec
@@ -24,6 +24,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 1.14'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
+  spec.add_development_dependency 'webmock', '~> 3.0.1'
 
   spec.add_runtime_dependency 'aws-sdk', '~> 2.9.37'
   spec.add_runtime_dependency 'rubyzip', '~> 1.2.1'
---
+++ lib/TerraformDevKit/request.rb
@@ -0,0 +1,28 @@
+require 'open-uri'
+require 'openssl'
+
+module TerraformDevKit
+  class Request
+    def initialize(url, query_strings: [], headers: {})
+      @url = url
+      @query_strings = query_strings
+      @headers = headers
+    end
+
+    def execute(raise_on_codes: [])
+      url = URI.parse(@url)
+      url.query = URI.encode_www_form(@query_strings) unless @query_strings.empty?
+      puts "Fetching #{url}"
+      options = {
+        redirect: false,
+        ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE
+      }
+      options.merge!(@headers)
+      open(url, options)
+    rescue OpenURI::HTTPError => error
+      response = error.io
+      raise if raise_on_codes.include?(response.status[0])
+      response
+    end
+  end
+end
