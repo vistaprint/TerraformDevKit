4822c24030e8e4025d82e29ac3de9a75d78b646e
Daniel Baker <dbaker@vistaprint.com>
2017-12-12 11:44:18 +0100

    Reverting to popen2e

--- lib/TerraformDevKit/command.rb
+++ lib/TerraformDevKit/command.rb
@@ -3,13 +3,8 @@ require 'open3'
 module TerraformDevKit
   class Command
     def self.run(cmd, directory: Dir.pwd, print_output: true)
-      Open3.popen3(cmd, chdir: directory) do |_, stdout, stderr, thread|
-        output = []
-        [stdout, stderr].each do |stream|
-          Thread.new {
-            output.concat process_output(stream, print_output)
-          }.join
-        end
+      Open3.popen2e(cmd, chdir: directory) do |_, stdout_and_stderr, thread|
+        output = process_output(stdout_and_stderr, print_output)
 
         thread.join
         raise "Error running command #{cmd}" unless thread.value.success?
