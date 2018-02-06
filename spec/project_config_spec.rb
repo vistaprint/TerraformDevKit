38c939d704962b4294ed5f9112a7b13dbb4e6750
Victor Jimenez <vjimenez@vistaprint.com>
2018-02-06 18:22:15 +0100

    Make naming more consistent with previous changes in this PR.

--- spec/project_config_spec.rb
+++ spec/project_config_spec.rb
@@ -1,21 +1,23 @@
 require 'TerraformDevKit/project_config'
 
+TDK = TerraformDevKit
+
 RSpec.describe TerraformDevKit::ProjectConfig do
   it 'should sanitise project name' do
-    terraform_project_config = TerraformDevKit::ProjectConfig.new('Some project Name')
+    project_config = TDK::ProjectConfig.new('Some project Name')
 
-    expect(terraform_project_config.name).to eq('some-project-name')
+    expect(project_config.name).to eq('some-project-name')
   end
 
   it 'should generate project acronym from project name' do
-    terraform_project_config = TerraformDevKit::ProjectConfig.new('Some project Name')
+    project_config = TDK::ProjectConfig.new('Some project Name')
 
-    expect(terraform_project_config.acronym).to eq('SPN')
+    expect(project_config.acronym).to eq('SPN')
   end
 
   it 'should use project acronym if given' do
-    terraform_project_config = TerraformDevKit::ProjectConfig.new('Some project Name', 'FOO')
+    project_config = TDK::ProjectConfig.new('Some project Name', 'FOO')
 
-    expect(terraform_project_config.acronym).to eq('FOO')
+    expect(project_config.acronym).to eq('FOO')
   end
 end
